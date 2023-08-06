a6d1b97957cc28732052c74f17450a121f067e44
Jose <joseramirezJose120@outlook.es>
2023-08-06 23:50:12 +0200

    added intersection points

--- prueba.py
+++ prueba.py
@@ -40,44 +40,55 @@ for line in lines:
     y1 = int(y0 + 1000*(a))
     x2 = int(x0 - 1000*(-b))
     y2 = int(y0 - 1000*(a))
-    if theta == 0:
-        cv.line(img, (x1,y1), (x2,y2),(0,0,255),1)
-    else:
-        cv.line(img, (x1,y1), (x2,y2),(0,255,0),1)
+    # if a == 1:
+    #     cv.line(img, (x1,y1), (x2,y2),(0,0,255),4)
+    # else:
+    #     cv.line(img, (x1,y1), (x2,y2),(0,255,0),4)
+        
 
 ####
 
-red = np.array([0, 0, 255])
-green = np.array([0, 255, 0])
-gx0, gy0, w = 0,0, 0
-
-for i, j in np.ndindex(img.shape[:-1]):
-    if np.array_equal(green, img[i,j]):
-        gx0, gy0 = i, j
-        break
-
-for i in range(gx0, img.shape[0]):
-    for j in range(gy0, img.shape[1]):
-        w += 1
-        if np.array_equal(red, img[i,j]):        
-            break
-    break
-
-print(w)
 
 
-cv.line(img, (gx0, gy0), (gx0 + w, gy0), (255, 0, 0), 1)
-cv.line(img, (gx0+w, gy0), (gx0 + w, gy0 + w), (255, 0, 0), 1)
-cv.line(img, (gx0, gy0), (gx0, gy0 + w), (255, 0, 0), 1)
-cv.line(img, (gx0, gy0 + w), (gx0 + w, gy0 + w), (255, 0, 0), 1)
+################# CLASSIFY
 
-for row in range(1, 9):
+angles = np.array([line[0][1] for line in lines])
+pts = np.array([[np.cos(angle), np.sin(angle)] for angle in angles], dtype=np.float32)
 
-    for i in range(1, 9):
-        cv.rectangle(img, (gx0 + (i*w), gy0 + (row*w)), (gx0 + (i*w) + w, gy0 + row*w + w), (255, 0, 0), 1)
-        cv.imshow('img', img)
-        cv.waitKey(0)
-        cv.destroyAllWindows()
+rows = []
+cols = []
+
+
+for line in lines:
+    if np.cos(line[0][1]) == 1:
+        cols.append(line)
+    else:
+        rows.append(line)
+
+
+
+group = [(r, c) for r in rows for c in cols]
+
+for tuple in group:
+    line1, line2 = tuple
+    rho1, theta1 = line1[0]
+    rho2, theta2 = line2[0]
+
+    A = np.array([
+        [np.cos(theta1), np.sin(theta1)],
+        [np.cos(theta2), np.sin(theta2)]
+    ])
+    b = np.array([[rho1], [rho2]])
+    x0, y0 = np.linalg.solve(A, b)
+    x0, y0 = int(np.round(x0)), int(np.round(y0))
+
+    cv.circle(img, (x0,y0), 4, (255,0, 0), 2)
+    
+
+
+
+
+###########################
 
 # min_area = 1400
 # max_area = 2000
