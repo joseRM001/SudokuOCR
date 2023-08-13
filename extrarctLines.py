88d54ed261956a2d894ee3d69a3716fe268f5575
Jose <joseramirezJose120@outlook.es>
2023-08-13 21:24:12 +0200

    get centroid

--- extrarctLines.py
+++ extrarctLines.py
@@ -6,7 +6,28 @@ def show_img(str, img):
     cv.waitKey(0)
     cv.destroyAllWindows()
 
+# return the centroid of a contour
+def get_centeroid(cnt):
+    length = len(cnt)
+    sum_x = np.sum(cnt[..., 0])
+    sum_y = np.sum(cnt[..., 1])
+    return int(sum_x / length), int(sum_y / length)
 
+def get_centers(img):
+    contours, hierarchies = cv.findContours(img, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+    for cnt in contours:
+        if cv.contourArea(cnt) > 100:
+            yield get_centeroid(cnt)
+
+
+
+def get_rows(img, centers, row_amt, row_h):
+    centers = np.array(centers)
+    d = row_h / row_amt
+    for i in range(row_amt):
+        f = centers[:, 1] - d * i
+        a = centers[(f < d) & (f > 0)]
+        yield a[a.argsort(0)[:, 0]]
 
 img = cv.imread('sudoku.png', cv.IMREAD_COLOR)
 assert img is not None, "file could not be read"
@@ -57,16 +78,31 @@ cv.rectangle(img, (x, y), (x + w, y + h), (36, 255, 12), 2)
 bw = bw[y:y+h, x:x+w]
 img = img[y:y+h, x:x+w]
 grid = grid[y:y+h, x:x+w]
-
-lines = cv.HoughLinesP(grid, 1, np.pi/180, 50, None, 50, 10)
-
-for i in range(0, len(lines)):
-    l = lines[i][0]
-    cv.line(img, (l[0], l[1]), (l[2], l[3]), (255, 0, 0), 3, cv.LINE_AA)
-    cv.circle(img, (l[0], l[1]), 2, (255, 0, 0), 2)
-    cv.circle(img, (l[2], l[3]), 2, (255, 0, 0), 2)
-
 ######
+
+
+contours, hier = cv.findContours(grid, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+cv.drawContours(img, contours, 1, (255, 255, 0), 5)
+count = 0
+for i in range(1, len(contours)):
+    cnt = contours[i]
+    if cv.contourArea(cnt) > 42*42:
+        x, y = get_centeroid(cnt)
+        cv.circle(img, (x, y), 10, (0, 0, 255), -1)
+        count += 1
+        cv.putText(img, str(count), (x-10, y+5), cv.FONT_HERSHEY_COMPLEX, 0.5, (0, 255, 255), 1)
+    
+# centers = list(get_centers(grid))
+# h, w = grid.shape
+# count = 0
+# for row in get_rows(img, centers, 4, h):
+#     cv.polylines(img, [row], False, (255, 0, 255), 2)
+#     for x, y in row:
+#         count += 1
+#         cv.circle(img, (x, y), 10, (0, 0, 255), -1)  
+#         cv.putText(img, str(count), (x - 10, y + 5), 1, cv.FONT_HERSHEY_PLAIN, (0, 255, 255), 2)
+# 
+
 
 show_img("img", img)
 
