ea911204ae9e2b903bae0bd08254906002f13d16
Jose <joseramirezJose120@outlook.es>
2023-08-13 23:05:50 +0200

    cells splitted

--- extrarctLines.py
+++ extrarctLines.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2 as cv
 import math
+import functools
 
 def show_img(str, img):
     cv.imshow(str, img)
@@ -14,8 +15,7 @@ def get_centeroid(cnt):
     sum_y = np.sum(cnt[..., 1])
     return int(sum_x / length), int(sum_y / length)
 
-
-img = cv.imread('sudoku2.png', cv.IMREAD_COLOR)
+img = cv.imread('sudoku.png', cv.IMREAD_COLOR)
 assert img is not None, "file could not be read"
 
 
@@ -30,9 +30,9 @@ else:
 ########## BINARIZE
 gray = cv.bitwise_not(gray)
 bw = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 15, -2)
-#show_img("bw", bw)
 
 
+######### PREPROCCESSING
 horizontal = np.copy(bw)
 vertical = np.copy(bw)
 
@@ -56,9 +56,7 @@ vertical = cv.dilate(vertical, vertical_SR)
 
 grid = cv.add(vertical, horizontal)
 contours, hier = cv.findContours(grid, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-#cv.drawContours(img, contours, -1, (36, 255, 12), 3)
 x, y, w, h = cv.boundingRect(contours[0])
-cv.rectangle(img, (x, y), (x + w, y + h), (36, 255, 12), 2)
 
 ###### CROP
 bw = bw[y:y+h, x:x+w]
@@ -74,35 +72,20 @@ cnts = []
 for i in range(1, len(contours)): # contour[0] is the external contour, we are interested in the internal contours
     cnt = contours[i]
     x, y = get_centeroid(cnt)
-    cv.circle(img, (x, y), 10, (0, 0, 255), -1)
     count += 1
-    cv.putText(img, str(count), (x-10, y+5), cv.FONT_HERSHEY_COMPLEX, 0.5, (0, 255, 255), 1)
+    # cv.circle(img, (x, y), 10, (0, 0, 255), -1) # DEBUG
+    # cv.putText(img, str(count), (x-10, y+5), cv.FONT_HERSHEY_COMPLEX, 0.5, (0, 255, 255), 1) # DEBUG
     cnts.append(cv.boundingRect(cnt))
     
-#cnts = sorted(cnts, key=lambda data:math.sqrt(data[0]**2 + data[1]**2))
-cnts = sorted(cnts, key=lambda data:data[1])
-cnts = sorted(cnts, key=lambda data:data[0])
+#cnts = sorted(cnts, key=lambda data:math.sqrt(data[0]**2 + data[1]**2)) # from origin
+cnts = sorted(cnts, key=lambda data:(data[0], data[1])) # SORTED BY COLUMN
 
+n = 0
 for i in range(0, len(cnts)):
     x, y, w, h = cnts[i]
-    cv.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-    show_img("img", img)
+    cell = img[y:y+h, x:x+w]
+    cv.imwrite("cells/Cell_{}.png".format(n), cell)
+    n += 1
+    # cv.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2) # DEBUG
 
 
-# centers = list(get_centers(grid))
-# h, w = grid.shape
-# count = 0
-# for row in get_rows(img, centers, 4, h):
-#     cv.polylines(img, [row], False, (255, 0, 255), 2)
-#     for x, y in row:
-#         count += 1
-#         cv.circle(img, (x, y), 10, (0, 0, 255), -1)  
-#         cv.putText(img, str(count), (x - 10, y + 5), 1, cv.FONT_HERSHEY_PLAIN, (0, 255, 255), 2)
-# 
-
-
-
-
-#result = cv.subtract(bw, result2)
-#show_img("morph", result)
-#show_img("morph", result2)
