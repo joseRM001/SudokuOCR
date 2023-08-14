10ab2cf3fc40bd7e0353e189032680df014ac97e
Jose <joseramirezJose120@outlook.es>
2023-08-14 13:49:11 +0200

    OCR on cells done

--- sudoku_ocr.cpp
+++ sudoku_ocr.cpp
@@ -1,12 +1,12 @@
+#include <cstdio>
 #include <leptonica/allheaders.h>
 #include <tesseract/baseapi.h>
 #include <iostream>
 #include <string>
 #include <format>
-#include <sys/types.h> // DELETE
-#include <dirent.h> // DELETE
 #include <filesystem>
-#include <algorithm>
+#include <cctype>
+
 
 const std::string PATH { "cells" };
 
@@ -16,16 +16,46 @@ unsigned count_files_in_directory(const std::string& path) {
     const std::filesystem::path dir_path{ path };
     unsigned i {};
     for (auto const& dir_entry : std::filesystem::directory_iterator{dir_path}) {
-        std::cout << dir_entry.path() << "\n";
         ++i;
     }
     return i;
 }
 
 int main() {
-    std::cout << count_files_in_directory(PATH) << std::endl;
-    PIX *pixs, *pixd;
-   
+    unsigned sudoku_size = count_files_in_directory(PATH);
+    unsigned n {1};
+  
+    tesseract::TessBaseAPI *api = new tesseract::TessBaseAPI();
+    if (api->Init(".", "eng")) {
+        perror("Could not initialize tesseract\n");
+        exit(1);
+    }
 
+    Pix *image;
+    for (unsigned i {1}; i <= sudoku_size; ++i) {
+        std::string filename = std::format("{}/Cell_{}.png", PATH, i);
+        image = pixRead(filename.c_str());
+        api->SetImage(image);
+        std::string outText = api->GetUTF8Text();
+        if (outText.size() >= 1 && std::isdigit(outText[0])) {
+            std::cout << outText[0] << " ";
+        } else {
+            std::cout << "0" << " ";
+        }
+        pixDestroy(&image);
+
+        if (i % 9 == 0) {
+            std::cout << "\n";
+        }
+    }
+
+
+
+
+    // FREEING RESOURCES
+    api->End();
+    delete api;
+    //delete[] outText;
+    //pixDestroy(&image);
     return 0;
 }
